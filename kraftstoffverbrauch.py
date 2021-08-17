8c27e4d941064700e73afd6d74b7f93ee7fa6367
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-17 22:47:54 +0200

    Add files via upload

---
+++ kraftstoffverbrauch.py
@@ -0,0 +1,11 @@
+print ("Kraftstoffverbrauch");
+
+liter = 1.95583
+
+km_input = float(input("km: "))
+
+print(km_input, "km")
+
+uliter = km_input * 0.069583333
+
+print(uliter,'l in', km_input,"km")
