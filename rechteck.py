77e09cb4bb8d6fb400d3414ef213e9110da5b5ad
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-18 08:54:29 +0200

    Add files via upload

---
+++ rechteck.py
@@ -0,0 +1,12 @@
+print ("Rechteck Berechnung");
+
+a = float(input("a: "))
+b = float(input("b: "))
+
+print("a: ", a)
+print("b: ", b)
+
+area = a * b
+umpfang = 2 * (a+b)
+print("area: ", area)
+print("umpfang  : ", umpfang)
